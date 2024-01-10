1c8219222a82a92b004d7b5f3e24486394a2c55b
Sneha22120605 <155344821+Sneha22120605@users.noreply.github.com>
2024-01-11 00:02:02 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,13 @@
+#python program to declare variables
+
+mynumber=3
+print(mynumber)
+
+mynumber2 = 4.5
+print(mynumber2)
+
+mynumber= "Hello world"
+print(mynumber)
+
+mynumber3= 'Hello world'
+print(mynumber3)
