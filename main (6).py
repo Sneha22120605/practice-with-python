81068bbf4f3ed080640b555d5c219c938176e213
Sneha <155344821+Sneha22120605@users.noreply.github.com>
2024-01-12 22:25:03 +0530

    Add files via upload

---
+++ main (6).py
@@ -0,0 +1,10 @@
+#python program to get input from the user
+#accepting integer from the user
+#the return type of input() function is string,
+#so we need to convert the input to integer
+
+num1 = int(input("Enter the num1 : "))
+num2 = int(input("Enter the num2 : "))
+
+num3 = num1 * num2
+print("The product is", num3)
