9f62a45adbcf6fc22781739971ecacd1842bf886
Sneha <155344821+Sneha22120605@users.noreply.github.com>
2024-01-12 22:34:38 +0530

    Add files via upload

---
+++ main (7).py
@@ -0,0 +1,10 @@
+#python program to illustrate 
+#selection statement 
+
+num1 = 22
+if(num1>12):
+    print("num1 is good")
+elif(num>22):
+    print("num1 is not good")
+else:
+    print("num1 is great")
