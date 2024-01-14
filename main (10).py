071d2c083b8bba66fefff1e3952ea23c426d1b97
Sneha <155344821+Sneha22120605@users.noreply.github.com>
2024-01-14 22:59:00 +0530

    Add files via upload

---
+++ main (10).py
@@ -0,0 +1,14 @@
+#python program to illustrate 
+#math module
+import math
+
+def main():
+    num = -85
+    
+    # fabs is used to get the absolute
+    #value of a decimal
+    num = math.fabs(num)
+    print(num)
+    
+
+main()
