251c2d68e14fc314f1705ef1550881ae4bb8a00a
Sneha <155344821+Sneha22120605@users.noreply.github.com>
2024-01-15 11:08:06 +0530

    Add files via upload

---
+++ main (13).py
@@ -0,0 +1,12 @@
+#python program to demonstrate 
+#creation of list
+
+#creating a list
+list = []
+print("blank list: ")
+print(list)
+
+#creating a list of numbers
+list = [10,20,30]
+print("\n list of numbers: ")
+print(list)
